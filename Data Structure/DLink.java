5572e7310473116c5e871e0e0088e17b3f2b2545
지윤혁 <yhji1127@gmail.com>
2022-05-06 00:00:52 +0900

    Add files via upload

---
+++ Data Structure/DLink.java
@@ -0,0 +1,14 @@
+class DLink<E> {
+    private E item;
+    private DLink<E> next;
+    private DLink<E> prev;
+    DLink(E item, DLink<E> prev, DLink<E> next) {
+        this.item = item; this.prev = prev; this.next = next;
+    }
+    DLink<E> next() { return next; }
+    DLink<E> setNext(DLink<E> next) { return this.next = next; }
+    DLink<E> prev() { return prev; }
+    DLink<E> setPrev(DLink<E> prev) { return this.prev = prev; }
+    E item() { return item; }
+    E setItem(E item) { return this.item = item; }
+}
