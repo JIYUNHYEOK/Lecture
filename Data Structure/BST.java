8ef37b210bedffd017581da8be1ace7fb45544a9
지윤혁 <yhji1127@gmail.com>
2022-05-19 15:57:26 +0900

    Add files via upload

---
+++ Data Structure/BST.java
@@ -0,0 +1,42 @@
+package btree;
+
+public class BST<K, E> implements Dictionary<K, E>{
+	
+	
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insert(K k, E e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public E remove(K k) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E removeAny() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E find(K k) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
